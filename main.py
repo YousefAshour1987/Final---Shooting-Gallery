73366ba0ffcb632e70eaf963127451ddedf15ea8
Your Name <ashouryousef96@gmail.com>
2026-05-15 16:33:26 -0500

    down 20

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import *
 import random
+import time
 
 ### CLASS and FUNCTION DEFINITIONS ###
 
@@ -20,7 +21,7 @@ def playing_area():
     pen.end_fill()
 
 class Player(Turtle):
-    def __init__(self, x, y, color, screen, right_key, left_key, fire_key, health):
+    def __init__(self, x, y, color, screen, right_key, left_key, fire_key, show_score):
         super().__init__()
         self.ht()
         self.speed(0)
@@ -32,12 +33,15 @@ class Player(Turtle):
         self.shape("turtle")
         self.bullets = []
         self.alive = True
+        self.score = 0
+        self.show_score = show_score
         self.st()
         screen.onkeypress(self.turn_left, left_key)
         screen.onkeypress(self.turn_right, right_key)
         screen.onkey(self.fire, fire_key)
     def turn_left(self):
         self.left(10)
+        
 
     def turn_right(self):
         self.right(10)
@@ -82,6 +86,26 @@ class Block(Turtle):
         self.goto(x, y)
         self.alive = True
         self.st()
+class Score(Turtle):
+    def __init__(self, x, y, player_name):
+        super().__init__()
+        self.ht()
+        self.speed(0)
+        self.penup()
+        self.goto(x, y)
+        self.color("white")
+        self.score = 0
+        self.player_name = player_name
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"{self.player_name}: {self.score}")
+
+    def add_score(self, points):
+        self.score += points
+        self.update_score()
+
 
 ### PROGRAM ###
 screen = Screen()
@@ -90,18 +114,36 @@ screen.bgcolor("black")
 
 screen.listen()
 
+start = time.time()
+
 playing_area()
 
 blocks = []
 
-p1 = Player(-100, 0, "red", screen, "d", "a", "w", 3)
-p2 = Player(100, 0, "blue", screen, "Right", "Left", "Up", 3)
-
-for y in range(190, 140, -20):
-    for x in range(-100, 120, 20):
+p1 = Player(-100, 0, "red", screen, "d", "a", "w", 0)
+p2 = Player(100, 0, "blue", screen, "Right", "Left", "Up", 0)
+def update():
+    if time.time() - start > 2:
+        start = time.time()
+        for block in blocks:
+            block.sety(block.ycor()-20)
+            blocks.append(Block(x, y))
+            for y in range(210, 160, -20):
+                for x in range(-220, 230, 20):
+                    blocks.append(Block(x, y))
+for y in range(210, 160, -20):
+    for x in range(-220, 230, 20):
         blocks.append(Block(x, y))
 
 while True:
+    screen.update()
+    if time.time() - start > 2:
+        start = time.time()
+        for block in blocks:
+            block.sety(block.ycor()-20)
+            blocks.append(Block(x, y))
+        
+
     for bullet in p1.bullets:
         bullet.move()
         if p2.distance(bullet) < 20:
